e06086943e182323960a9b5b27fc28606b1fa1df
= <pezi10002@hotmail.com>
2020-11-11 14:54:03 +0100

    Ausgangsprojekt mit H2 DB Config ready

---
+++ src/test/java/com/pezi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pezi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
